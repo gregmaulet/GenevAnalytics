2632c98f406295a58b6e66d4621e44330f7c14a4
JimmyDang14 <Jimmy.Dang@etu.unige.ch>
2020-05-14 18:03:38 +0200

    Modified DataAnalysis (rounding, notification scientifique)

--- Data_Analysis.R
+++ Data_Analysis.R
@@ -1,6 +1,6 @@
 
 rm(list = ls())
-
+options(scipen=999)
 
 library(dplyr)
 
@@ -238,63 +238,63 @@ axis(1, at=1:20, labels=2000:2019)}
 ##5) FONCTIONS POUR OBTENIR DONNEES VOULUES D'UNE COMMUNE EN DERNIERE ANNEE DISPONIBLE------------------------------------------------------
 #5.1) DI : Depenses d'investissement de la commmune par habitant (2017)
 freturnDI <- function(a){x <- ratioDI[c(a),19]
-return(x)
+return(round(x, digits = 2))
 }
 #5.2) Loyermoyen100m2 : Loyer moyen d'un appartement de 100m2 (2019)
 freturnloyer <- function(a){x <- Loyermoyen100m2_06_19_OK[c(a),15]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.3) Ratio Ecole : Nombre d'ecoles par km2 : 
 freturnecole <- function(a){x <- ratio_ecole[c(a), c(2)]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.4) Population : Nombre d'habitants (2019) 
 freturnpopulation <- function(a){x <- NHabitants_OK[c(a),21]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.5) Ratio Infraction pour 1000 habitants
 # Nombre d'infractions du Code Penal pour 1000 habitants (2019)
 freturninfraction <- function(a){x <- ratioInfraction[c(a),13]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.6) Ratio Medecin (pour 1000 habitants) : Nombre de medecins pour 1000 habitants
 freturnmedecin <- function(a){x <- ratioMedecins[c(a), c(2)]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.7) Ratio Parc : Nombre de parcs par km2
 freturnparc <- function(a){x <- ratio_NParcs[c(a), c(2)]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.8) Ratio Supermarche : Nombre de supermarchés par km2
 freturnsupermarche <- function(a){x <- ratio_Supermarches[c(a), c(2)]
 cat("Nombre de supermarchés par km2 : ") 
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.9) Prix mediane appartement 100m2 : Prix mediane d'un appartement de 100m2 (2017)
 freturnprixappart <- function(a){x <- PMedianeAppart100m2_00_17_OK[c(a),19]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.10) Prix mediane Villa : Prix mediane d'une villa (Taille inconnue, 2017)
 freturnprixvilla <- function(a){x <- PMedianeVilla_00_17_OK[c(a),19]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.11) Densite population : Nombre d'habitants par km2 (2019) 
 freturndensitepopulation <- function(a){x <- Densite_population[c(a),21]
-return(x)
+return(round(x, digits = 2))
 }
 
 #5.12) Superficie km2 : Superficie de la commune en km2
 freturnsuperficie <- function(a){x <- Superficie_OK[c(a), c(2)]
-return(x)
+return(round(x, digits = 2))
 }
 
 ##6) CLEAN USELESS DATA------------------------------------------------------
